af3843bd2c7b6a2415b5a37903de71e924f4c066
niiquash <qua16006@byui.edu>
2022-11-21 21:35:03 -0700

    added json server and mock json data for fetch api practice

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react'
+import AddItem from './AddItem';
+import SearchItem from './SearchItem';
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: 'Item 1'
-    },
-    {
-      id: 2,
-      checked: false,
-      item: 'Item 2'
-    },
-    {
-      id: 3,
-      checked: false,
-      item: 'Item 3'
-    },
-  ])
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || [])
+
+  const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('shoppinglist', JSON.stringify(items))
+  }, [items])
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1
+    const mynewItem = { id, checked: false, item }
+    const listItems = [...items, mynewItem]
+    setItems(listItems)
+  }
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
     setItems(listItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id)
     setItems(listItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!newItem) return
+    // addItem
+    addItem(newItem)
+    setNewItem('')
+    console.log('submitted')
   }
 
   return (
     <div className="App">
       <Header title='Grocery List' />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         setItems={setItems}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
